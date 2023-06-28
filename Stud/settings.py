d549ae5613f384a7f9db4da29051fcbb21991eb5
javiste <mamadou12ndir@gmail.com>
2023-06-28 02:48:50 +0000

    firs commit1

--- Stud/settings.py
+++ Stud/settings.py
@@ -135,13 +135,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
